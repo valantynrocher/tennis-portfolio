d54a81befe6f0afbcf2e87a35b304ff59675b5be
valantynrocher <valentinrocher@gmail.com>
2021-07-03 01:18:34 +0200

    sync themeName with localstorage

--- src/context/AppProvider.tsx
+++ src/context/AppProvider.tsx
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MuiThemeProvider } from "@material-ui/core";
 import { getThemeByName, ThemeNameType } from "../themes/index";
 import { AppContext } from "./AppContext";
 
 const AppProvider = (props: { children: React.ReactNode }) => {
   // State to hold the selected theme name
-  const [themeName, setThemeName] = useState<ThemeNameType>("Paris");
+  const [themeName, setThemeName] = useState<ThemeNameType>(
+    (localStorage.getItem("themeName") as ThemeNameType) || "Paris"
+  );
   const [drawerOpen, setDrawerOpen] = useState(false);
   const [quizOpen, setQuizOpen] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("themeName", themeName);
+  }, [themeName]);
 
   // Retrieve the theme object by theme name
   const theme = getThemeByName(themeName);
